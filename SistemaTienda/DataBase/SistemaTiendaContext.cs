b4d5d21632dee88a091100c4c7c0c411a2b0af5a
hugoa <hugoa@HUGO-AQUINO>
2024-04-09 22:38:49 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTienda/DataBase/SistemaTiendaContext.cs
@@ -0,0 +1,26 @@
+﻿using SistemaTienda.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda.DataBase
+{
+    public class SistemaTiendaContext : DbContext
+    {
+        public SistemaTiendaContext() : base("name = SistemaTienda")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+        public DbSet<Cliente> clientes { get; set; }
+    }
+}
---
+++ SistemaTienda/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SistemaTienda.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SistemaTienda.DataBase.SistemaTiendaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SistemaTienda.DataBase.SistemaTiendaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
